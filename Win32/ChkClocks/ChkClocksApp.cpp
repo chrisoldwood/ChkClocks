e796ab15044a2393700a469dd64b7c9eb2df9f57
Administrator <administrator@chrisoldwood.com>
2003-01-13 11:41:23 +0000

    Updated version number.

--- Win32/ChkClocks/ChkClocksApp.cpp
+++ Win32/ChkClocks/ChkClocksApp.cpp
@@ -29,9 +29,9 @@ CChkClocksApp App;
 */
 
 #ifdef _DEBUG
-const char* CChkClocksApp::VERSION      = "v1.0 Beta [Debug]";
+const char* CChkClocksApp::VERSION      = "v1.0 [Debug]";
 #else
-const char* CChkClocksApp::VERSION      = "v1.0 Beta";
+const char* CChkClocksApp::VERSION      = "v1.0";
 #endif
 const char* CChkClocksApp::INI_FILE_VER     = "1.0";
 const int   CChkClocksApp::DEF_THREADS      = 10;
