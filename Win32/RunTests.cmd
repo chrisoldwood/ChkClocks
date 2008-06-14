3c7ddd935eba53c2a10b400aab7cac799614704a
Admin <admin@chrisoldwood.com>
2008-06-14 21:29:29 +0000

    Added COMTrace and DDECOMClient tests. Added support for running either the debug or release build tests.

--- Win32/RunTests.cmd
+++ Win32/RunTests.cmd
@@ -5,10 +5,25 @@ REM Script to execute all Unit Tests.
 REM
 REM ************************************************************
 
+:check_build
+IF /I "%1" == "" GOTO :invalid_args
+
+:set_debug_build
+IF /I NOT "%1" == "debug" GOTO :set_release_build
+SET BUILD=Debug
+GOTO :run_tests
+
+:set_release_build
+IF /I NOT "%1" == "release" GOTO :invalid_args
+SET BUILD=Release
+GOTO :run_tests
+
+:run_tests
+
 ECHO ========================================
 ECHO Running 'Core' library tests...
 ECHO ========================================
-Lib\Core\Test\Debug\Test.exe
+Lib\Core\Test\%BUILD%\Test.exe
 IF errorlevel 1 goto :failed
 
 ECHO.
@@ -16,7 +31,7 @@ ECHO.
 ECHO ========================================
 ECHO Running 'WCL' library tests...
 ECHO ========================================
-Lib\WCL\Test\Debug\Test.exe
+Lib\WCL\Test\%BUILD%\Test.exe
 IF errorlevel 1 goto :failed
 
 ECHO.
@@ -24,7 +39,7 @@ ECHO.
 ECHO ========================================
 ECHO Running 'NCL' library tests...
 ECHO ========================================
-Lib\NCL\Test\Debug\Test.exe
+Lib\NCL\Test\%BUILD%\Test.exe
 IF errorlevel 1 goto :failed
 
 ECHO.
@@ -32,7 +47,7 @@ ECHO.
 ECHO ========================================
 ECHO Running 'MDBL' library tests...
 ECHO ========================================
-Lib\MDBL\Test\Debug\Test.exe
+Lib\MDBL\Test\%BUILD%\Test.exe
 IF errorlevel 1 goto :failed
 
 ECHO.
@@ -40,7 +55,7 @@ ECHO.
 ECHO ========================================
 ECHO Running 'COM' library tests...
 ECHO ========================================
-Lib\COM\Test\Debug\Test.exe
+Lib\COM\Test\%BUILD%\Test.exe
 IF errorlevel 1 goto :failed
 
 ECHO.
@@ -48,7 +63,23 @@ ECHO.
 ECHO ========================================
 ECHO Running 'XML' library tests...
 ECHO ========================================
-Lib\XML\Test\Debug\Test.exe
+Lib\XML\Test\%BUILD%\Test.exe
+IF errorlevel 1 goto :failed
+
+ECHO.
+
+ECHO ========================================
+ECHO Running 'COMTrace' library tests...
+ECHO ========================================
+COMTrace\Test\%BUILD%\Test.exe
+IF errorlevel 1 goto :failed
+
+ECHO.
+
+ECHO ========================================
+ECHO Running 'DDECOMClient' library tests...
+ECHO ========================================
+DDECOMClient\Test\%BUILD%\Test.exe
 IF errorlevel 1 goto :failed
 
 ECHO.
@@ -60,5 +91,8 @@ goto :done
 ECHO.
 ECHO ERROR: The suite of tests FAILED.
 goto :done
+
+:invalid_args
+ECHO Usage: RunTests [debug or release]
 
 :done
