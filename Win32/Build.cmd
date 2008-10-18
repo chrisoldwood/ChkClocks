da128fb95c0f000bcd5e0603b4c901f15d5b0b17
Admin <admin@chrisoldwood.com>
2008-10-18 20:41:50 +0000

    Added check that the compiler vars are set. Made formatting consistent.

--- Win32/Build.cmd
+++ Win32/Build.cmd
@@ -1,89 +1,105 @@
 @ECHO OFF
 REM ************************************************************
 REM
-REM Script to build a single solution for both Debug and Release
-REM targets.
+REM Script to build a single solution for both Debug and/or
+REM Release targets.
 REM
 REM NB: You must set the environment variables first by running
-REM the appropriate script:-
-REM
-REM VC++ 7.1: "%VS71COMNTOOLS%\vsvars32.bat"
-REM VC++ 8.0: "%VS80COMNTOOLS%\vsvars32.bat"
-REM VC++ 9.0: "%VS90COMNTOOLS%\vsvars32.bat"
-REM
-REM If building with STLport you also need to set the STLport
-REM paths as well by running:-
-REM
-REM STLport\SetVars.cmd
+REM the SetVars script. If building with STLport you need to use
+REM the STLport\SetVars script instead.
 REM
 REM ************************************************************
 
-:check_compiler
-IF /I "%1" == "" GOTO :invalid_args
+REM 
+REM Check command line..
+REM
 
-:check_solution
+:check_args
+IF /I "%1" == "" GOTO :invalid_args
+IF /I "%2" == "" GOTO :invalid_args
 IF /I "%3" == "" GOTO :invalid_args
+
+REM 
+REM Check environment variables are set.
+REM
+
+:check_vars
+IF /I "%VCVERSION%" == "" GOTO :invalid_vars
 
 REM 
 REM Determine compiler.
 REM
+
 :set_compiler
 IF /I "%1" == "vc71" GOTO :do_vc71
 IF /I "%1" == "vc80" GOTO :do_vc80
 IF /I "%1" == "vc90" GOTO :do_vc90
-goto :invalid_args
+GOTO :invalid_args
 
 :do_vc71
 SET VSBUILD=VC71
-goto :do_debug
+GOTO :do_debug
+
 :do_vc80
 SET VSBUILD=VC80
-goto :do_debug
+GOTO :do_debug
+
 :do_vc90
 SET VSBUILD=VC80
-goto :do_debug
+GOTO :do_debug
 
 REM
 REM Do a Debug build?
 REM
+
 :do_debug
 IF /I "%2" == ""      GOTO :invalid_args
 IF /I "%2" == "both"  GOTO :debug_build
 IF /I "%2" == "debug" GOTO :debug_build
-goto :do_release
+GOTO :do_release
 
 :debug_build
-IF "%VSBUILD%" == "VC80" goto :vc80_debug_build
-IF "%VSBUILD%" == "VC90" goto :vc90_debug_build
+IF "%VSBUILD%" == "VC80" GOTO :vc80_debug_build
+IF "%VSBUILD%" == "VC90" GOTO :vc90_debug_build
 devenv /nologo /useenv %3 /build Debug
-goto :do_release
+GOTO :do_release
 
 :vc80_debug_build
 :vc90_debug_build
 vcbuild /nologo /useenv %3 "Debug|Win32"
-goto :do_release
+GOTO :do_release
 
 REM
 REM Do a Release build?
 REM
+
 :do_release
 IF /I "%2" == ""        GOTO :invalid_args
 IF /I "%2" == "both"    GOTO :release_build
 IF /I "%2" == "release" GOTO :release_build
-goto :done
+GOTO :done
 
 :release_build
-IF "%VSBUILD%" == "VC80" goto :vc80_release_build
-IF "%VSBUILD%" == "VC90" goto :vc90_release_build
+IF "%VSBUILD%" == "VC80" GOTO :vc80_release_build
+IF "%VSBUILD%" == "VC90" GOTO :vc90_release_build
 devenv /nologo /useenv %3 /build Release
-goto :done
+GOTO :done
 
 :vc80_release_build
 :vc90_release_build
 vcbuild /nologo /useenv %3 "Release|Win32"
-goto :done
+GOTO :done
+
+REM 
+REM Report errors.
+REM
 
 :invalid_args
 ECHO Usage: Build [vc71 or vc80 or vc90] [debug or release or both] [solution]
+GOTO :done
+
+:invalid_vars
+ECHO ERROR: Compiler environment variables not set. Run 'SetVars' first.
+GOTO :done
 
 :done
--- Win32/BuildAll.cmd
+++ Win32/BuildAll.cmd
@@ -9,18 +9,39 @@ REM variables up first.
 REM
 REM ************************************************************
 
+REM 
+REM Check command line..
+REM
+
 :check_args
 IF /I "%1" == "" GOTO :invalid_args
 IF /I "%2" == "" GOTO :invalid_args
 
+REM 
+REM Check environment variables are set.
 REM
-REM Do all builds
+
+:check_vars
+IF /I "%VCVERSION%" == "" GOTO :invalid_vars
+
 REM
+REM Do builds
+REM
+
 :do_builds
-FOR /R %%I IN (*.sln) DO CALL Build %1 %2 %%I
-goto :done
+FOR /R %%I IN (*.sln) DO ECHO %%I & CALL Build %1 %2 %%I
+GOTO :done
+
+REM 
+REM Report errors.
+REM
 
 :invalid_args
 ECHO Usage: BuildAll [vc71 or vc80 or vc90] [debug or release or both]
+GOTO :done
+
+:invalid_vars
+ECHO ERROR: Compiler environment variables not set. Run 'SetVars' first.
+GOTO :done
 
 :done
