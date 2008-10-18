af8eaf936a78244b93fb7c9f1ca29743bf1bc221
Admin <admin@chrisoldwood.com>
2008-10-18 20:37:27 +0000

    Added code to set VCVERSION so that we can detect if the vars have been set.

--- Win32/SetVars.cmd
+++ Win32/SetVars.cmd
@@ -13,12 +13,18 @@ REM
 REM Check command line..
 REM
 
-:check_compiler
+:check_args
 IF /I "%1" == "" GOTO :invalid_args
 
 REM 
 REM Remove existing variables.
 REM
+
+:check_vars
+IF /I "%VCVERSION%" == "" GOTO :clean
+ECHO.
+ECHO WARNING: Overwriting variables for '%VCVERSION%'
+ECHO.
 
 :clean
 SET INCLUDE=
@@ -33,23 +39,33 @@ REM
 IF /I "%1" == "vc71" GOTO :do_vc71
 IF /I "%1" == "vc80" GOTO :do_vc80
 IF /I "%1" == "vc90" GOTO :do_vc90
-goto :invalid_args
+GOTO :invalid_args
 
 REM 
 REM Setup variables.
 REM
 
 :do_vc71
-"%VS71COMNTOOLS%\vsvars32.bat"
-goto :done
+CALL "%VS71COMNTOOLS%\vsvars32.bat"
+SET VCVERSION=vc71
+GOTO :done
+
 :do_vc80
-"%VS80COMNTOOLS%\vsvars32.bat"
-goto :done
+CALL "%VS80COMNTOOLS%\vsvars32.bat"
+SET VCVERSION=vc80
+GOTO :done
+
 :do_vc90
-"%VS90COMNTOOLS%\vsvars32.bat"
-goto :done
+CALL "%VS90COMNTOOLS%\vsvars32.bat"
+SET VCVERSION=vc90
+GOTO :done
+
+REM 
+REM Report errors.
+REM
 
 :invalid_args
 ECHO Usage: SetVars [vc71 or vc80 or vc90]
+GOTO :done
 
 :done
