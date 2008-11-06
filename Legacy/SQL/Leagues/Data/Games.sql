5e094eaf389f8b2724965d270517360941ee5377
Admin <admin@chrisoldwood.com>
2008-11-06 22:17:50 +0000

    Vss2Git

---
+++ Legacy/SQL/Leagues/Data/Games.sql
@@ -0,0 +1,7 @@
+DELETE FROM Leagues
+DELETE FROM Games
+
+INSERT INTO Games(ID, Name) VALUES (1, 'Darts')
+INSERT INTO Games(ID, Name) VALUES (2, 'Football')
+INSERT INTO Games(ID, Name) VALUES (3, 'Pool')
+INSERT INTO Games(ID, Name) VALUES (4, 'Rugby')
